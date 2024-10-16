bbea1f4c9cca6ceb34aaf433823f0bfe58a8ae43
The_Kohan <piotr.kochanowski95@gmail.com>
2024-10-16 11:49:16 +0200

    feat: add test cases

--- maestro-test/src/test/kotlin/maestro/test/IntegrationTest.kt
+++ maestro-test/src/test/kotlin/maestro/test/IntegrationTest.kt
@@ -50,6 +50,7 @@ import java.awt.Color
 import java.io.File
 import java.nio.file.Paths
 import kotlin.system.measureTimeMillis
+import javax.imageio.ImageIO
 
 class IntegrationTest {
 
@@ -4416,6 +4417,43 @@ class IntegrationTest {
                 events.add(CallbackEvent("onCommandSkipped", uniqueIndex, getSequence()))
             },
         )
+    }
+
+    @Test
+    fun `Case 122 - Take cropped screenshot`() {
+        // Given
+        val commands = readCommands("122_take_cropped_screenshot")
+        val boundHeight = 100
+        val boundWidth = 100
+
+        val driver = driver {
+            element {
+                id = "element_id"
+                bounds = Bounds(0,0,boundHeight,boundWidth)
+            }
+        }
+
+        val device = driver.deviceInfo()
+        val dpr = device.heightPixels / device.heightGrid
+
+        // When
+        Maestro(driver).use {
+            orchestra(it).runFlow(commands)
+        }
+
+        // Then
+        // No test failure
+        driver.assertEvents(
+            listOf(
+                Event.TakeScreenshot,
+            )
+        )
+        val file = File("122_take_cropped_screenshot_with_filename.png")
+        val image = ImageIO.read(file)
+
+        assert(file.exists())
+        assert(image.width == (boundWidth * dpr))
+        assert(image.height == (boundHeight * dpr))
     }
 
     private fun orchestra(
