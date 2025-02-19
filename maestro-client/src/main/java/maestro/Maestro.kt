ed8257f2170ebd83a8153d348140cf30e9285afb
MarcellDr <marcelldr@yahoo.com>
2025-02-19 22:02:23 +0700

    fix: Missmatch between screenshotBasedTap and hierarchyBasedTap, assign function to correct its implementation.

--- maestro-client/src/main/java/maestro/Maestro.kt
+++ maestro-client/src/main/java/maestro/Maestro.kt
@@ -288,7 +288,7 @@ class Maestro(
         }
     }
 
-    private fun screenshotBasedTap(
+    private fun hierarchyBasedTap(
         x: Int,
         y: Int,
         retryIfNoChange: Boolean = true,
@@ -297,7 +297,7 @@ class Maestro(
         tapRepeat: TapRepeat? = null,
         waitToSettleTimeoutMs: Int? = null
     ) {
-        LOGGER.info("Tapping at ($x, $y) using screenshot based logic for wait")
+        LOGGER.info("Tapping at ($x, $y) using hierarchy based logic for wait")
 
         val hierarchyBeforeTap = initialHierarchy ?: viewHierarchy()
 
@@ -323,8 +323,9 @@ class Maestro(
             }
         }
     }
+    
 
-    private fun hierarchyBasedTap(
+    private fun screenshotBasedTap(
         x: Int,
         y: Int,
         retryIfNoChange: Boolean = true,
@@ -333,7 +334,7 @@ class Maestro(
         tapRepeat: TapRepeat? = null,
         waitToSettleTimeoutMs: Int? = null
     ) {
-        LOGGER.info("Tapping at ($x, $y) using hierarchy based logic for wait")
+        LOGGER.info("Try tapping at ($x, $y) using hierarchy based logic for wait")
 
         val hierarchyBeforeTap = initialHierarchy ?: viewHierarchy()
         val screenshotBeforeTap: BufferedImage? = ScreenshotUtils.tryTakingScreenshot(driver)
@@ -360,6 +361,8 @@ class Maestro(
                 LOGGER.info("Something have changed in the UI judging by view hierarchy. Proceed.")
                 return
             }
+
+            LOGGER.info("Tapping at ($x, $y) using screenshot based logic for wait")
 
             val screenshotAfterTap: BufferedImage? = ScreenshotUtils.tryTakingScreenshot(driver)
             if (screenshotBeforeTap != null &&
