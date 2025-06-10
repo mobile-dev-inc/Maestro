b164b7b80d149505d35ba208c32b8936fe7f8bf8
felipeduarte <felipeduartea04@gmail.com>
2025-06-10 16:56:21 -0300

    adding cause to errors

--- maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
@@ -934,8 +934,7 @@ class Orchestra(
             maestro.setPermissions(command.appId, permissions)
 
         } catch (e: Exception) { 
-            logger.error("Failed clearing app state", e)
-            throw MaestroException.UnableToClearState("Unable to clear state for app ${command.appId}: ${e.message}")
+            throw MaestroException.UnableToClearState("Unable to clear state for app ${command.appId}: ${e.message}", e)
         }
 
         try {
@@ -945,7 +944,7 @@ class Orchestra(
                 stopIfRunning = command.stopApp ?: true
             )
         } catch (e: Exception) {
-            throw MaestroException.UnableToLaunchApp("Unable to launch app ${command.appId}: ${e.message}")
+            throw MaestroException.UnableToLaunchApp("Unable to launch app ${command.appId}: ${e.message}", e)
         }
 
         return true
