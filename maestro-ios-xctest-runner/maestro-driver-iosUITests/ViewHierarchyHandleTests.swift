0225137488f276e7486cf9516916a937d0c2d12c
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2025-06-16 13:35:52 +0530

    feat: added github action for integration tests

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/ViewHierarchyHandleTests.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/ViewHierarchyHandleTests.swift
@@ -22,6 +22,8 @@ final class ViewHierarchyHandlerTests: XCTestCase {
     
     func testAppOffsetAdjustsCorrectly() async throws {
         // given
+        let testApp = await XCUIApplication(bundleIdentifier: "org.wikimedia.wikipedia")
+        await testApp.launch()
         guard let url = URL(string: "http://localhost:8080/hierarchy") else {
             throw NSError(domain: "XCTestError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Failed to construct URL"])
         }
@@ -31,7 +33,6 @@ final class ViewHierarchyHandlerTests: XCTestCase {
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let springboardApp = await XCUIApplication(bundleIdentifier: "com.apple.springboard")
-        let testApp = await XCUIApplication(bundleIdentifier: "org.wikimedia.wikipedia")
         let springboardFrame = await springboardApp.frame
         let testAppFrame = await testApp.frame
         
