38750c5d4f50f9634711b493f6756351f9b4e340
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2025-07-10 01:28:16 +0530

    chore: update minimum java version to 17

--- maestro-orchestra-models/build.gradle.kts
+++ maestro-orchestra-models/build.gradle.kts
@@ -12,13 +12,13 @@ mavenPublishing {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.named("compileKotlin", KotlinCompilationTask::class.java) {
     compilerOptions {
-        freeCompilerArgs.addAll("-Xjdk-release=1.8")
+        freeCompilerArgs.addAll("-Xjdk-release=17")
     }
 }
 
