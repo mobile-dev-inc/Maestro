8292226b2d30bdd1a03941918d37b0412eecaebb
Proksh Luthra <35415752+proksh@users.noreply.github.com>
2025-07-24 16:16:32 +0530

    fix: added routing import

--- maestro-cli/src/main/java/maestro/cli/auth/Auth.kt
+++ maestro-cli/src/main/java/maestro/cli/auth/Auth.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.runBlocking
 import maestro.auth.ApiKey
