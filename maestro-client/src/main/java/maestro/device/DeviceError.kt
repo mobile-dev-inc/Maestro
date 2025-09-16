0b52711417728cdf5cb53318a5104f36d3e11403
Matthew de Detrich <mdedetrich@gmail.com>
2025-09-16 13:32:06 +0200

    chore: Put last error in device exception (#2727)

--- maestro-client/src/main/java/maestro/device/DeviceError.kt
+++ maestro-client/src/main/java/maestro/device/DeviceError.kt
@@ -4,4 +4,4 @@ package maestro.device
  * Exception class specifically for device-related errors in the client module.
  * Functionally equivalent to CliError in maestro-cli.
  */
-class DeviceError(override val message: String) : RuntimeException(message)
+class DeviceError(override val message: String, override val cause: Throwable? = null) : RuntimeException(message, cause)
