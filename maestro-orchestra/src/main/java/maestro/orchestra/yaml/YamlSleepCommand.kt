8f56b6291f75aa93122ef7ad1df9ce6da358ef8b
Rasyid Ridho <rasyidridho7@gmail.com>
2025-11-13 17:41:42 +0700

    ref sleep, seconds: 5, ms: 5000

--- maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlSleepCommand.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlSleepCommand.kt
@@ -1,14 +1,21 @@
 package maestro.orchestra.yaml
 import com.fasterxml.jackson.annotation.JsonCreator
-data class YamlSleepCommand(
+data class YamlSleepCommand internal constructor(
     val time: Long?,
     val label: String? = null,
 ) {
     companion object {
         @JvmStatic
         @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-        fun create(time: Long): YamlSleepCommand {
-            return YamlSleepCommand(time = time)
+        fun fromLong(time: Long): YamlSleepCommand {
+            return YamlSleepCommand(time, null)
+        }
+
+        @JvmStatic
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        fun fromObject(ms: Long?, seconds: Long?, label: String?): YamlSleepCommand {
+            val time = ms ?: seconds?.let { it * 1000 }
+            return YamlSleepCommand(time, label)
         }
     }
 }
