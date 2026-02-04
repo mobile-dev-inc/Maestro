af34d06534e42f0707843ab179ad84bc9dfe1527
Brad Anderson <brad@sankatygroup.com>
2026-02-04 18:43:07 -0500

    refactor: extract shared drag gesture utility and cleanup
    - Remove unused 'steps' variable in AndroidDriver.drag()
    - Extract duplicate drag implementation from WebDriver and CdpWebDriver
      to shared SeleniumGestures.performDrag() utility
    - Add DRAG_LONG_PRESS_DURATION_MS constant (1000ms) for clarity

--- maestro-client/src/main/java/maestro/drivers/WebDriver.kt
+++ maestro-client/src/main/java/maestro/drivers/WebDriver.kt
@@ -14,6 +14,7 @@ import maestro.TreeNode
 import maestro.ViewHierarchy
 import maestro.device.Platform
 import maestro.utils.ScreenshotUtils
+import maestro.utils.SeleniumGestures
 import maestro.web.input.inputHtmlDate
 import maestro.web.input.isHtmlDateInput
 import maestro.web.record.JcodecVideoEncoder
@@ -497,39 +498,7 @@ class WebDriver(
 
     override fun drag(start: Point, end: Point, durationMs: Long) {
         val driver = ensureOpen()
-
-        val finger = PointerInput(PointerInput.Kind.TOUCH, "finger")
-        val drag = org.openqa.selenium.interactions.Sequence(finger, 1)
-        // Move to start position
-        drag.addAction(
-            finger.createPointerMove(
-                Duration.ofMillis(0),
-                PointerInput.Origin.viewport(),
-                start.x,
-                start.y
-            )
-        )
-        // Press and hold
-        drag.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
-        // Pause to simulate long press before drag (1 second)
-        drag.addAction(finger.createPointerMove(
-            Duration.ofMillis(1000),
-            PointerInput.Origin.viewport(),
-            start.x,
-            start.y
-        ))
-        // Drag to end position
-        drag.addAction(
-            finger.createPointerMove(
-                Duration.ofMillis(durationMs - 1000),
-                PointerInput.Origin.viewport(),
-                end.x,
-                end.y
-            )
-        )
-        // Release
-        drag.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()))
-        (driver as RemoteWebDriver).perform(listOf(drag))
+        SeleniumGestures.performDrag(driver as RemoteWebDriver, start, end, durationMs)
     }
 
     override fun backPress() {
