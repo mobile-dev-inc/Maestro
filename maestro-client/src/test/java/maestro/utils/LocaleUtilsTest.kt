97a5a350d858f8338d47b6546dc4ca4ad573b899
Proksh Luthra <35415752+proksh@users.noreply.github.com>
2026-02-19 18:17:18 +0530

    fix platform import

--- maestro-client/src/test/java/maestro/utils/LocaleUtilsTest.kt
+++ maestro-client/src/test/java/maestro/utils/LocaleUtilsTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import com.google.common.truth.Truth.assertThat
 import kotlin.DeprecationLevel
+import maestro.device.Platform
 
 @Deprecated(
     message = "LocaleUtilsTest is deprecated. LocaleUtils is deprecated and will be removed in a future version. Please use DeviceLocaleTest (maestro.locale.DeviceLocaleTest) instead.",
