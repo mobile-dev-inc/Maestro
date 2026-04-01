44cb012ff6bf6942dd0961d8a9a408eaf0ee271f
Proksh Luthra <35415752+proksh@users.noreply.github.com>
2026-04-01 23:30:52 +0530

    Enrich AppMetadata as single source of truth for app analysis (#3112)
    * feat: added fields to AppMetadata to complete response
    
    * fix: remove AppValidationResult and simplified things

--- maestro-orchestra/src/main/java/maestro/orchestra/util/AppMetadataAnalyzer.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/util/AppMetadataAnalyzer.kt
@@ -4,7 +4,6 @@ import com.dd.plist.NSDictionary
 import com.dd.plist.PropertyListParser
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import maestro.device.AppValidationResult
 import maestro.device.Platform
 import net.dongliu.apk.parser.ApkFile
 import java.io.File
@@ -14,21 +13,21 @@ import java.util.zip.ZipFile
 
 object AppMetadataAnalyzer {
 
-    fun validateAppFile(file: File): AppValidationResult? {
+    fun validateAppFile(file: File): AppMetadata? {
         getWebMetadata(file)?.let {
-            return AppValidationResult(Platform.WEB, it.url)
+            return it
         }
         getIosAppMetadata(file)?.let {
             require(it.platformName == "iphonesimulator") {
                 "App build target '${it.platformName}' not supported, set build target to 'iphonesimulator'"
             }
-            return AppValidationResult(Platform.IOS, it.bundleId)
+            return it
         }
         getAndroidAppMetadata(file)?.let {
             require(it.supportedArchitectures.isEmpty() || "arm64-v8a" in it.supportedArchitectures) {
                 "APK does not support arm64-v8a architecture. Found: ${it.supportedArchitectures}"
             }
-            return AppValidationResult(Platform.ANDROID, it.packageId)
+            return it
         }
         return null
     }
@@ -63,6 +62,8 @@ object AppMetadataAnalyzer {
                             ?: throw NullPointerException("Unable to find DTPlatformName"),
                         minimumOSVersion = root.objectForKey("MinimumOSVersion")?.toString()
                             ?: throw NullPointerException("Unable to find MinimumOSVersion"),
+                        appVersion = root.objectForKey("CFBundleShortVersionString")?.toString() ?: "",
+                        bundleVersion = root.objectForKey("CFBundleVersion")?.toString() ?: "",
                     )
                 }
             }
@@ -85,7 +86,13 @@ object AppMetadataAnalyzer {
                             .distinct().toList()
                     }
                 } catch (_: Exception) { emptyList() }
-                return AndroidAppMetadata(meta.packageName, archs)
+                return AndroidAppMetadata(
+                    name = meta.name ?: "",
+                    packageId = meta.packageName,
+                    supportedArchitectures = archs,
+                    versionName = meta.versionName ?: "",
+                    versionCode = meta.versionCode ?: 0L,
+                )
             }
         } catch (_: IOException) {
             return null
@@ -99,16 +106,49 @@ object AppMetadataAnalyzer {
     }
 }
 
+sealed class AppMetadata(
+    open val name: String,
+    val appIdentifier: String,
+    val platform: Platform,
+    val internalVersion: String,
+    val version: String,
+)
+
 data class IosAppMetadata(
-    val name: String,
+    override val name: String,
     val bundleId: String,
     val platformName: String,
     val minimumOSVersion: String,
+    val appVersion: String,
+    val bundleVersion: String,
+) : AppMetadata(
+    name = name,
+    appIdentifier = bundleId,
+    platform = Platform.IOS,
+    internalVersion = bundleVersion,
+    version = appVersion,
 )
 
 data class AndroidAppMetadata(
+    override val name: String,
     val packageId: String,
     val supportedArchitectures: List<String>,
+    val versionName: String,
+    val versionCode: Long,
+) : AppMetadata(
+    name = name,
+    appIdentifier = packageId,
+    platform = Platform.ANDROID,
+    internalVersion = versionCode.toString(),
+    version = versionName,
 )
 
-data class WebAppMetadata(val url: String)
+data class WebAppMetadata(
+    val url: String,
+) : AppMetadata(
+    name = url,
+    appIdentifier = url,
+    platform = Platform.WEB,
+    internalVersion = "",
+    version = "",
+)
