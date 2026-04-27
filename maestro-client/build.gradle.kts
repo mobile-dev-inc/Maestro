4edf8f276a905def81e809d30937c919ccf0d136
Amanjeet Singh <amanjeetsingh150@gmail.com>
2026-04-27 12:55:50 +0530

    fix(ios-driver): fail-fast on transport failure instead of rethrowing raw SocketTimeoutException (#3212)

--- maestro-client/build.gradle.kts
+++ maestro-client/build.gradle.kts
@@ -170,6 +170,7 @@ dependencies {
     testImplementation(libs.google.truth)
     testImplementation(libs.square.mock.server)
     testImplementation(libs.junit.jupiter.params)
+    testImplementation(libs.mockk)
 }
 
 java {
